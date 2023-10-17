5d4a7718031000495e028d8a5ac540236effdecb
ifham <mail2ifham@gmail.com>
2023-10-18 05:09:46 +0530

    setup ejs

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+
+
+const app = express()
+const port= 3000
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true}))
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+app.get("/work", (req, res) => {
+    res.render("index.ejs")
+})
+
+
+app.listen(port, () => {
+    console.log(`Listen to port ${port}...`);
+} )
