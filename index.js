b45fe56053955df91a10d1da69e160b390683557
ifham <mail2ifham@gmail.com>
2023-11-03 05:41:16 +0530

    finnal commit

--- index.js
+++ index.js
@@ -2,29 +2,44 @@ import express from "express";
 
 const app = express();
 const port = 3000;
-const workList = [];
-const todayList = [];
+const workList = []; //work list array
+const todayList = []; //today list array
 
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public")); //set static file's folder
+app.use(express.urlencoded({ extended: true })); // set bodyParser
 
+// home route which is handle today list
 app.get("/", (req, res) => {
+  // render index.ejs file with todayList
   res.render("index.ejs", { itemList: todayList });
 });
+// work route which is handle work list
 app.get("/work", (req, res) => {
+  // render work.ejs file with workList
   res.render("work.ejs", { itemList: workList });
 });
 
-
+// route for add item
 app.post("/addItem", (req, res) => {
-  const key = Object.keys(req.body)[0];
-  const newItem = req.body[key];
-  if (key === "today-add-felid") {
-    todayList.push(newItem);
-    res.redirect("/");
-  } else {
-    workList.push(newItem);
-    res.redirect("/work");
+  const key = Object.keys(req.body)[0]; // get the name felid value
+  const newItem = req.body[key]; // get the new item
+  if (newItem !== "") {  //avoid empty entry
+    if (key === "today-add-felid") {
+      todayList.push(newItem); //add to todayList array
+      res.redirect("/"); // go back to todayList
+    } else {
+      //   console.log("work",newItem);
+      workList.push(newItem); //add to workList array
+      res.redirect("/work"); //go back to workList array
+    }
+  }else {
+    if (key === "today-add-felid") {
+        res.redirect("/"); // go back to todayList
+      } else {
+        //   console.log("work",newItem);
+        res.redirect("/work"); //go back to workList array
+      }
+  
   }
 });
 
